fee5da80c0f57e7805b059b97ace4e158d01fb70
Bruno Azevedo <contato.cheble@gmail.com>
2020-02-09 14:44:43 +0000

    [Backend] - Instalação do websocket; - Desenvolvimento de uma função, para calcular a distancia entre duas coordenadas; - Quando é inserido um novo Dev na aplicação, o socket verifica se também deve exibir este Dev para os utilizadores que estão buscando na App;

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
 
 module.exports = {
     async index(req, res) {
@@ -30,6 +31,15 @@ module.exports = {
                 techs: techsArray,
                 location
             });
+
+            // filtrar as conexões que estão há no máximo 10km de distancia
+            // e que o novo dev tenha pelo menos uma das tecnologias filtradas
+            const sendSocketMessageTo = findConnections(
+                { latitude, longitude },
+                techsArray
+            );
+
+            sendMessage(sendSocketMessageTo, 'new-dev', dev);
         }
 
         return res.json(dev);
